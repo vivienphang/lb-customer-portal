964eb9235139ef34c8d2930af43e3ea8f7bb7a28
Vivien <vivienphang@gmail.com>
2023-06-27 13:46:21 +0800

    adding component: Login UI done

---
+++ src/components/login.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Container, TextField, Button } from "@mui/material";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+
+  return (
+    <>
+      <Container maxWidth="xs" fixed={true}>
+        <form onSubmit={loginHandler}>
+          <TextField
+            sx={{ height: 40 }}
+            label="Email"
+            type="email"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            margin="normal"
+          />
+          <TextField
+            sx={{ height: 40 }}
+            label="Password"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            size="large"
+            sx={{ marginTop: "2rem", marginBottom: "3rem" }}
+          >
+            Login
+          </Button>
+        </form>
+      </Container>
+
+    </>
+  );
+};
+
+export default Login;
